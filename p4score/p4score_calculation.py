e90c0c1c78c3f137bcf9f99562c281304fb44744
Thomas Vidori <thomas.vidori@protonmail.com>
2025-02-24 14:59:09 +0100

    Fix wrong check on null values

--- p4score/p4score_calculation.py
+++ p4score/p4score_calculation.py
@@ -5,7 +5,7 @@ def calculate_p4score(
         false_positive: int,
         true_negative: int,
 ) -> float:
-    if true_positive == 0 or false_negative == 0:
+    if true_positive == 0 or true_negative == 0:
         return 0.0
 
     precision = true_positive / (true_positive + false_positive)
@@ -16,4 +16,3 @@ def calculate_p4score(
     return (4 / (
         1 / precision + 1 / recall + 1 / specificity + 1 / npv
     ))
-
