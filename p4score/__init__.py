4541a30ea60e145a51106108391fb3f836027583
Thomas Vidori <thomas.vidori@protonmail.com>
2025-02-25 15:39:24 +0100

    Add __version__ attribute

--- p4score/__init__.py
+++ p4score/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("p4score")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from p4score.p4score_calculation import (
     calculate_p4score,
     calculate_p4score_from_predictions,
